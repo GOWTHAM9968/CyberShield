e9555e564e9d539d84a24dd28ce3bd07327dc3ab
gowtham <gg4749467@gmail.com>
2026-06-30 21:37:53 +0530

    Day 14 Email Alerts and Notification System

--- app.py
+++ app.py
@@ -364,3 +364,12 @@ def delete_user(id):
     conn.close()
 
     return redirect("/users")
+from flask_mail import Mail, Message
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "your_email@gmail.com"
+app.config["MAIL_PASSWORD"] = "your_app_password"
+
+mail = Mail(app)
